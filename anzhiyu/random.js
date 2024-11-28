25e90e024d52b79bdf843e9dc26e5d1798e86645
KD-Y <2490922948@qq.com>
2024-11-28 13:10:26 +0800

    Site updated: 2024-11-28 13:10:26

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2024/11/03/Geely/","2024/10/20/hello-world/","2024/11/27/vue/"];function toRandomPost(){
+var posts=["2024/11/03/Geely/","2024/11/27/vue/","2024/10/20/hello-world/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
